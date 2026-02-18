e2288e002cbf42db1e4f97bb52a757ed5a2bf13b
Majzoub <majzoubalsiddig@gmail.com>
2026-02-18 21:48:30 +0200

    completed the contact page

--- frontend/src/app/contact/components/RenderContacts.tsx
+++ frontend/src/app/contact/components/RenderContacts.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import "./style.css";
 
 type Contact = {
   name: string;
@@ -10,26 +9,35 @@ type Contact = {
   img: string;
 };
 
-type contactsProps = { contacts: Contact[]; };
+type contactsProps = { contacts: Contact[] };
 
 const RenderContacts: React.FC<contactsProps> = ({ contacts }) => {
   return (
-    <div className="courses-container">
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 m-10">
       {contacts.map((contact, index) => (
-        <a
+        <div
           key={index}
-          href={contact.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="contact-card"
+          className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col items-center p-6 hover:shadow-lg transition"
         >
-          <img src={contact.img} alt={contact.name} className="contact-img" />
-          <div className="contact-info">
-            <h3 className="contact-name font-bold">{contact.name}</h3>
-            <p className="contact-role text-blue-300">{contact.role}</p>
-            <p className="contact-about">{contact.about}</p>
+          <img
+            src={contact.img}
+            alt={contact.name}
+            className="w-64 h-50 object-cover border-3 border-blue-400 rounded-[15]"
+          />
+          <div className="mt-6">
+            <h1 className="text-center text-2xl font-bold text-blue-500">{contact.name}</h1>
+            <h3 className="text-center text-lg text-blue-400">{contact.role}</h3>
+            <p className="m-3 text-gray-600 text-sm leading-relaxed">{contact.about}</p>
           </div>
-        </a>
+          <a
+            href={contact.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-3 px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 transition"
+          >
+            Contact {contact.name}
+          </a>
+        </div>
       ))}
     </div>
   );
--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,57 +1,51 @@
 import Footer from "@/components/Footer";
 import Header from "./components/Header";
-import RenderCourses from "./components/RenderContacts";
+import RenderContacts from "./components/RenderContacts";
 
-const testingCoursesData = [
+const testingContactsData = [
     {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
+        name: "The contact name",
+        role: "The contact role",
+        about: "The contact about lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        link: "The contact link",
+        img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
     },
     {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
+        name: "The contact name",
+        role: "The contact role",
+        about: "The contact about lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        link: "The contact link",
+        img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
     },
     {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
+        name: "The contact name",
+        role: "The contact role",
+        about: "The contact about lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        link: "The contact link",
+        img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
     },
     {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
+        name: "The contact name",
+        role: "The contact role",
+        about: "The contact about lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        link: "The contact link",
+        img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
     },
     {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
+        name: "The contact name",
+        role: "The contact role",
+        about: "The contact about lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        link: "The contact link",
+        img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
     },
     {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
-    },
-    {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
-    },
-    {
-        name: "Course Name",
-        description: "this is the course description, it should be a bit long to test the layout and see how it looks like when we have a lot of text, but it should not be too long to avoid breaking the layout and making it look bad.",
-        link: "htttps://google.com",
-        cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
+        name: "The contact name",
+        role: "The contact role",
+        about: "The contact about lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        link: "The contact link",
+        img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?auto=format&fit=crop&q=80"
     }
-];
+]
 
 const Contact = () => {
     return (
@@ -60,7 +54,7 @@ const Contact = () => {
             <div className="w-full gap-3">
                 {/* <h2 className="text-3xl text-center m-10 text-blue-500 font-bold">Over the 2 Chapters, those are the Contact we dedicated our time and effort.</h2> */}
                 <h3 className="text-2xl text-center m-10 text-blue-500 font-bold">Contact</h3>
-                {/* <RenderContact Contact={testingContactData} /> */}
+                <RenderContacts contacts={testingContactsData} />
                 <Footer />
             </div>
         </div>
