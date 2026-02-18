5fd95fccdc0c815ca7cad0bf5cd964b9c94243c7
baboshi <drhameid@yahoo.com>
2026-02-18 15:31:54 +0200

    Basic Admin Page

---
+++ frontend/src/app/admin/add/page.tsx
@@ -0,0 +1,7 @@
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
+
+const AddArticlePage = () => {
+  return <SimpleEditor />;
+};
+
+export default AddArticlePage;
--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,5 +1,21 @@
-const Admin = () => {
-  return <div>admin</div>;
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="text-center">
+      <Link href="/admin/add">Add Article</Link>
+      <h1>Available articles</h1>
+      <div className=" my-auto mx-auto grid grid-cols-5 gap-3">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, index) => (
+          <div key={index} className="h-40 w-40 border border-black">
+            <h1>Article: {index + 1}</h1>
+            <h1>Edit</h1>
+            <h1>Delete</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Admin;
+export default page;
